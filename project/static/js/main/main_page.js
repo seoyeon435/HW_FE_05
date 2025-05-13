ee860434ed947fb590bbf98b39218772a3bc832b
seoyeon435 <0305psy@naver.com>
2025-05-13 15:30:56 +0900

    마켓컬리 클론코딩 제출

--- project/static/js/main/main_page.js
+++ project/static/js/main/main_page.js
@@ -1,1 +1,12 @@
-/* main page - JS */
+const categoryLinks = document.querySelectorAll('.category-bar a');
+
+
+categoryLinks.forEach(link => {
+    link.addEventListener('mouseenter', () => {
+        link.classList.add('hovered');
+    });
+
+    link.addEventListener('mouseleave', () => {
+        link.classList.remove('hovered');
+    });
+});
