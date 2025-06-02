914d78aaa97ffd811b5cfd47ccb5c64006788fd9
seoyeon435 <0305psy@naver.com>
2025-06-02 16:23:20 +0900

    HW_FE_05

--- project/static/js/accounts/login.js
+++ project/static/js/accounts/login.js
@@ -1,1 +1,24 @@
 /* login page - JS */
+document.addEventListener("DOMContentLoaded", function () {
+    const idInput = document.getElementById("id");
+    const pwInput = document.getElementById("password");
+    const loginBtn = document.getElementById("login-btn");
+
+    function validateLoginForm() {
+        const idFilled = idInput.value.trim() !== "";
+        const pwFilled = pwInput.value !== "";
+
+        const isValid = idFilled && pwFilled;
+
+        
+        loginBtn.disabled = !isValid;
+    }
+
+    
+    idInput.addEventListener("input", validateLoginForm);
+    pwInput.addEventListener("input", validateLoginForm);
+});
+
+
+
+console.log("Login page JS loaded");
--- project/static/js/accounts/signup.js
+++ project/static/js/accounts/signup.js
@@ -1,1 +1,71 @@
 /* signup page - JS */
+
+
+// 변수 선언하기기
+const idInput = document.getElementById("id");
+const pwInput = document.getElementById("password");
+const pwCheckInput = document.getElementById("password-check");
+const signupBtn = document.getElementById("signup-btn");
+const pwMismatchMsg = document.getElementById("pw-mismatch-msg");
+
+
+// 비밀번호 검사
+function checkPassword(password) {
+    const minLength = /.{8,}/;           
+    const upper = /[A-Z]/;               
+    const lower = /[a-z]/;             
+    const number = /[0-9]/;              
+    const symbol = /[@!?_-]/;  
+    
+    return (
+        minLength.test(password) &&
+        upper.test(password) &&
+        lower.test(password) &&
+        number.test(password) &&
+        symbol.test(password) 
+    );
+
+}
+
+
+// 전체 검사사
+function checkForm() {
+    const id = idInput.value;
+    const pw = pwInput.value;
+    const pwCheck = pwCheckInput.value;
+    const bottomBox = document.querySelector(".bottom");
+
+
+    if (pwCheck !== "" && pw !== pwCheck) {
+        pwMismatchMsg.style.display = "block";
+    } else {
+        pwMismatchMsg.style.display = "none";
+    }
+
+    const isValid =
+        id !== "" &&
+        pw !== "" &&
+        pw === pwCheck &&
+        checkPassword(pw);
+
+    // signupBtn.disabled = !isValid;
+    // signupBtn.style.backgroundColor = isValid ? "#000" : "#5E0080";
+    // signupBtn.style.color = isValid ? "#fff" : "#999";
+    
+    if (isValid) {
+        signupBtn.style.background = "var(--point-main, #5E0080)";
+        signupBtn.style.color = "#FFF";
+        bottomBox.style.background = "var(--point-main, #5E0080)"; 
+    } else {
+        signupBtn.style.background = "var(--stroke-stroke-2, #E2E2E2)";
+        signupBtn.style.color = "var(--font-font2, #999)";
+        bottomBox.style.background = "var(--stroke-stroke-2, #E2E2E2)"; 
+    }
+
+}
+
+
+idInput.addEventListener("input", checkForm);
+pwInput.addEventListener("input", checkForm);
+pwCheckInput.addEventListener("input", checkForm);
+
